5f158113e08cb76c4c4266858d4440f9e794c111
Harshit Shukla <harshitshukla@Harshits-MacBook-Air.local>
2026-07-30 20:11:00 +0530

    add the vinresponse

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool(
   {
-    connectionString: `postgressql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_NAME}`
+    connectionString: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:15432/${process.env.DB_NAME}`
 
     // connectionString: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@13.202.193.4:15432/${process.env.DB_NAME}`
 
@@ -25,4 +25,3 @@ module.exports = {
     }
   }
 };
-
